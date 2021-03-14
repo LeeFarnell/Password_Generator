6fb7d3b917aa7fb1ec6c04c450d41a4759bb7141
Lee Farnell <leefarnell@mac.com>
2021-03-14 23:50:03 +0000

    created to do comments list

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// TO DO
+// Create Arrays for LowerCase, UpperCase, Numbers & Special Characters
+// Length Validation Prompt: no less than 8, no more than 128. Alert if not true.
+// Confirm if Lowercase wanted
+// Confirm if Uppercase wanted
+// Confirm if Numbers wanted
+// Confirm if special characters wanted
+// Alert if all false.
