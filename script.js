31369c13bf796d464d294858f4e822127f277acb
Lee Farnell <leefarnell@mac.com>
2021-03-15 10:24:47 +0000

    added numbers array

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ generateBtn.addEventListener("click", writePassword);
 // Create Arrays for LowerCase, UpperCase, Numbers & Special Characters
 const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 // Length Validation Prompt: no less than 8, no more than 128. Alert if not true.
 // Confirm if Lowercase wanted
