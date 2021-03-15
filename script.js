95ee837177e525b08cc829973345fe46715d606e
Lee Farnell <leefarnell@mac.com>
2021-03-15 10:40:50 +0000

    added password legnth promt

--- script.js
+++ script.js
@@ -27,9 +27,10 @@ generateBtn.addEventListener("click", writePassword);
 const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-const specials = ['!', '\"', '#', '$', '%', '&', '\'', '()', '*', '+', ',', '-', '.', '/', ':', ';' '<', '=', '>', '?', '@' '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '\"']
+const specials = ['!', '\"', '#', '$', '%', '&', '\'', '()', '*', '+', ',', '-', '.', '/', ':', ';' '\<', '=', '>', '?', '@' '\[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '\"']
 
 // Length Validation Prompt: no less than 8, no more than 128. Alert if not true.
+length = prompt("How long would you like your password to be? Please select a number between 8 - 128")
 // Confirm if Lowercase wanted
 // Confirm if Uppercase wanted
 // Confirm if Numbers wanted
