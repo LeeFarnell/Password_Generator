58bbe3dc395594e7c2940edfc82f2bbe64e85035
Lee Farnell <leefarnell@mac.com>
2021-03-15 12:04:20 +0000

    amended arrays

--- script.js
+++ script.js
@@ -1,15 +1,113 @@
+const lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const specials = [
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "()",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+  '"',
+];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-	// Create a variable to store our generated password
-	var password = ''
+  // Create a variable to store our generated password
+  var password = "";
 
-	// Logic would go here
-	...
+  // Logic would go here
+  // Length Validation Prompt: no less than 8, no more than 128. Alert if not true.
 
-	// Return our created password
-	return password;
+  // Confirm if Lowercase wanted
+  // Confirm if Uppercase wanted
+  // Confirm if Numbers wanted
+  // Confirm if special characters wanted
+  // Alert if all false.
+
+  // Return our created password
+  return password;
 }
 // Write password to the #password input
 function writePassword() {
@@ -21,17 +119,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// TO DO
-// Create Arrays for LowerCase, UpperCase, Numbers & Special Characters
-const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-const specials = ['!', '\"', '#', '$', '%', '&', '\'', '()', '*', '+', ',', '-', '.', '/', ':', ';' '\<', '=', '>', '?', '@' '\[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '\"']
-
-// Length Validation Prompt: no less than 8, no more than 128. Alert if not true.
-// Confirm if Lowercase wanted
-// Confirm if Uppercase wanted
-// Confirm if Numbers wanted
-// Confirm if special characters wanted
-// Alert if all false.
