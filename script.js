434285c34e0ef03a92703c4d317b15052464122c
Lee Farnell <leefarnell@mac.com>
2021-03-15 10:23:47 +0000

    added upper case array

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ generateBtn.addEventListener("click", writePassword);
 // TO DO
 // Create Arrays for LowerCase, UpperCase, Numbers & Special Characters
 const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 // Length Validation Prompt: no less than 8, no more than 128. Alert if not true.
 // Confirm if Lowercase wanted
