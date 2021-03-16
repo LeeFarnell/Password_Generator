59b78444534cffb2a3709004978fd2a0b92ceeab
Lee Farnell <leefarnell@mac.com>
2021-03-16 10:29:57 +0000

    Confirm statements placed within If statement

--- script.js
+++ script.js
@@ -105,62 +105,63 @@ function generatePassword() {
   // Before if convert string to number Number.parseInt() and store in new variable.
   const passwordParsed = Number.parseInt(passwordLength);
   // Put confirm statements within this IF block.
-  // Generate password after Confirm statements and If Block.
-  // Create Options Arrays.
+
   if (passwordParsed >= 8 && passwordParsed <= 128) {
-    console.log(passwordLength);
+    console.log(passwordParsed);
+    // Confirm if Lowercase wanted
+    const confirmLower = confirm(
+      "Do you want to use any lowercase characters? Press 'OK' for yes, 'Cancel' for no."
+    );
+    if (confirmLower == true) {
+      console.log("True");
+    } else {
+      console.log("False");
+    }
+    // Confirm if Uppercase wanted
+    const confirmUpper = confirm(
+      "Do you want to use any uppercase characters? Press 'OK' for yes, 'Cancel' for no."
+    );
+    if (confirmUpper == true) {
+      console.log("True");
+    } else {
+      console.log("False");
+    }
+    // Confirm if Numbers wanted
+    const confirmNumber = confirm(
+      "Do you want to use any numbers? Press 'OK' for yes, 'Cancel' for no."
+    );
+    if (confirmNumber == true) {
+      console.log("True");
+    } else {
+      console.log("False");
+    }
+    // Confirm if special characters wanted
+    const confirmSpecial = confirm(
+      "Do you want to use any special characters? Press 'OK' for yes, 'Cancel' for no."
+    );
+    if (confirmSpecial == true) {
+      console.log("True");
+    } else {
+      console.log("False");
+    }
+    // Alert if all false.
+    if (
+      confirmLower == false &&
+      confirmUpper == false &&
+      confirmNumber == false &&
+      confirmSpecial == false
+    ) {
+      alert("You must select at least one of the character types.");
+      console.log("Try again");
+    }
   } else {
     alert("Number Invalid. Please insert a number between 8-128.");
     console.log("False");
     return;
   }
 
-  // Confirm if Lowercase wanted
-  const confirmLower = confirm(
-    "Do you want to use any lowercase characters? Press 'OK' for yes, 'Cancel' for no."
-  );
-  if (confirmLower == true) {
-    console.log("True");
-  } else {
-    console.log("False");
-  }
-  // Confirm if Uppercase wanted
-  const confirmUpper = confirm(
-    "Do you want to use any uppercase characters? Press 'OK' for yes, 'Cancel' for no."
-  );
-  if (confirmUpper == true) {
-    console.log("True");
-  } else {
-    console.log("False");
-  }
-  // Confirm if Numbers wanted
-  const confirmNumber = confirm(
-    "Do you want to use any numbers? Press 'OK' for yes, 'Cancel' for no."
-  );
-  if (confirmNumber == true) {
-    console.log("True");
-  } else {
-    console.log("False");
-  }
-  // Confirm if special characters wanted
-  const confirmSpecial = confirm(
-    "Do you want to use any special characters? Press 'OK' for yes, 'Cancel' for no."
-  );
-  if (confirmSpecial == true) {
-    console.log("True");
-  } else {
-    console.log("False");
-  }
-  // Alert if all false.
-  if (
-    confirmLower == false &&
-    confirmUpper == false &&
-    confirmNumber == false &&
-    confirmSpecial == false
-  ) {
-    alert("You must select at least one of the character types.");
-    console.log("Try again");
-  }
+  // Generate password after Confirm statements and If Block.
+  // Create Options Arrays.
 
   // If Lower is True and all others are false
   // If Upper is True and all others are false
